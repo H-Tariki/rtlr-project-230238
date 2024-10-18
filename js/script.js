dc65d5b26387883414c7372028f139e7d5a0ff3e
Habib Mirzaei <mr.habib.mirzaei.tr@gmail.com>
2024-10-18 19:48:01 +0330

    set a responsive slider with swipers library

--- js/script.js
+++ js/script.js
@@ -6,3 +6,50 @@ left.addEventListener('mouseover' , ()=>{
     }, 2000);
 })
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    speed: 1000,
+
+    autoplay:{
+        delay:2000,
+    },
+
+    slidesPerView: 5,
+    spaceBetween: 50,
+
+    breakpoints: {
+        1400:{
+            slidesPerView: 5,
+        },
+        1000:{
+            slidesPerView: 4,
+        },
+        500:{
+            slidesPerView: 3,
+        },
+        200:{
+            slidesPerView: 2,
+        },
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+
