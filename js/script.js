11d5fbc8d635cbf335bf2f0d9e03db2a8bb605b5
Habib Mirzaei <mr.habib.mirzaei.tr@gmail.com>
2024-10-24 13:51:22 +0330

    set responsive portfolio section

--- js/script.js
+++ js/script.js
@@ -52,5 +52,94 @@ const swiper = new Swiper('.swiper', {
     },
   });
 
+  // __________________
 
+  let portfoItem1 = document.getElementById("portfolio-1")
+  let portfoItem1Img=document.getElementById("portfolio-1-img")
+  let darken1= document.getElementById("darken1")
+  portfoItem1.addEventListener('mouseenter' , ()=>{
+    portfoItem1Img.classList.add("scaling")
+    setTimeout(() => {
+      portfoItem1Img.classList.add("rotating")
+    }, 200);
+    darken1.classList.remove("darkness")
+    portfoItem1.classList.add("bordering")
+    setTimeout(() => {
+      portfoItem1Img.classList.remove("scaling")
+      portfoItem1Img.classList.add("onscaling")
+      darken1.classList.add("z")
+    }, 400);
+  })
+  portfoItem1.addEventListener('mouseleave' , ()=>{
+    portfoItem1Img.classList.remove("onscaling")
+    portfoItem1Img.classList.add("scaling")
+    setTimeout(() => {
+      portfoItem1Img.classList.remove("scaling")
+      portfoItem1Img.classList.remove("rotating")
+    }, 300);
+    setTimeout(() => {
+      portfoItem1.classList.remove("bordering")
+      darken1.classList.add("darkness")
+    }, 400);
+  })
+// ..................
+  let portfoItem2 = document.getElementById("portfolio-2")
+  let portfoItem2Img=document.getElementById("portfolio-2-img")
+  let darken2= document.getElementById("darken2")
+  portfoItem2.addEventListener('mouseenter' , ()=>{
+    portfoItem2Img.classList.add("scaling")
+    setTimeout(() => {
+      portfoItem2Img.classList.add("rotating")
+    }, 200);
+    darken2.classList.remove("darkness")
+    portfoItem2.classList.add("bordering")
+    setTimeout(() => {
+      portfoItem2Img.classList.remove("scaling")
+      portfoItem2Img.classList.add("onscaling")
+      darken2.classList.add("z")
+    }, 400);
+  })
+  portfoItem2.addEventListener('mouseleave' , ()=>{
+    portfoItem2Img.classList.remove("onscaling")
+    portfoItem2Img.classList.add("scaling")
+    setTimeout(() => {
+      portfoItem2Img.classList.remove("scaling")
+      portfoItem2Img.classList.remove("rotating")
+    }, 300);
+    setTimeout(() => {
+      portfoItem2.classList.remove("bordering")
+      darken2.classList.add("darkness")
+    }, 400);
+  })
+
+  // ___________________________________
+
+  let portfoItem3 = document.getElementById("portfolio-3")
+  let portfoItem3Img=document.getElementById("portfolio-3-img")
+  let darken3= document.getElementById("darken3")
+  portfoItem3.addEventListener('mouseenter' , ()=>{
+    portfoItem3Img.classList.add("scaling")
+    setTimeout(() => {
+      portfoItem3Img.classList.add("rotating")
+    }, 200);
+    darken3.classList.remove("darkness")
+    portfoItem3.classList.add("bordering")
+    setTimeout(() => {
+      portfoItem3Img.classList.remove("scaling")
+      portfoItem3Img.classList.add("onscaling")
+      darken3.classList.add("z")
+    }, 400);
+  })
+  portfoItem3.addEventListener('mouseleave' , ()=>{
+    portfoItem3Img.classList.remove("onscaling")
+    portfoItem3Img.classList.add("scaling")
+    setTimeout(() => {
+      portfoItem3Img.classList.remove("scaling")
+      portfoItem3Img.classList.remove("rotating")
+    }, 300);
+    setTimeout(() => {
+      portfoItem3.classList.remove("bordering")
+      darken3.classList.add("darkness")
+    }, 400);
+  })
 
